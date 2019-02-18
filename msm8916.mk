90fae3caa158f7ff14d7c632fbf2f389610fe59c
Mrinal Ghosh <mg712702@gmail.com>
2019-02-18 05:24:55 +0000

    msm8916-common: Cleanup common tree
    * Based On https://github.com/lettuce-pie/android_device_yu_lettuce/tree/1dbd90edaf3e61360ba6f237300194fbd31e5881
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -17,13 +17,6 @@
 # inherit from the proprietary version
 $(call inherit-product, vendor/asus/msm8916-common/msm8916-common-vendor.mk)
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
@@ -316,11 +309,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     sensors.msm8916
 
-# Strip debug
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
-
 # Telephony-ext
-PRODUCT_PACKAGES += \
+wPRODUCT_PACKAGES += \
     telephony-ext \
     ims-ext-common \
     libshims_ims
